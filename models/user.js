d72a86640e0ddc594de638673ca4487b45612609
Dylan <dburston0@gmail.com>
2022-03-20 11:52:51 -0400

    added models/user.js, signup functionality working

---
+++ models/user.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    password: {
+      type: String,
+      trim: true,
+      minLength: 3,
+      required: true
+    },  
+  },{
+    timestamps: true,
+    toJSON: {
+      transform: function(doc, ret) {
+        delete ret.password;
+        return ret;
+      }
+    }
+  }
+);
+
+module.exports = mongoose.model("User", userSchema);
